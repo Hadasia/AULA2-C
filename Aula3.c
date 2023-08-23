848f124c437fc9a448380e036b306ccb4d29c732
Hadasia <hadasiasousa@gmail.com>
2023-08-22 21:10:52 -0300

    calculadora3

---
+++ Aula3.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+
+int main(){
+int valor1;
+int valor2;
+int resultado;
+char operacao;
+
+valor1 = 10;
+valor2 = 2;
+
+//SOMA: resultado = valor1 + valor2;
+//SUBTRAÇÃO: resultado = valor1 - valor2;
+//MULTIPLICAÇÃO: resultado = valor1 + valor2;
+//DIVISÃO: resultado = valor1 / valor2;
+//RESTO: resultado = valor1 % valor2;
+
+printf("Insira uma operacao:\n");
+scanf("%c", &operacao);
+
+printf("insira 2 valores:\n");
+printf("Valor 1:\n");
+scanf("%d", &valor1);
+printf("Valor2:\n");
+scanf("%d", &valor2);
+
+if(operacao == '+'){
+resultado = valor1 + valor2;
+
+}
+else if(operacao == '-'){
+resultado = valor1 - valor2;
+
+}
+if(operacao == '*'){
+resultado = valor1 * valor2;
+
+}
+else if(operacao == '/'){
+resultado = valor1 / valor2;
+
+}
+else{
+resultado = valor1 % valor2;
+
+}
+
+
+
+printf("resultado:%d\n", resultado);
+printf("operacao: %c\n", operacao);
+
+    return 0;
+}
